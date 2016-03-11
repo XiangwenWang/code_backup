285bff1727b0a9947d96f58093239b4171c3d1bf
Xiangwen Wang <wxw@vt.edu>
2016-03-11 12:58:45 -0500

    new run

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
-quantity_list = ['d', 's', 'l', 't', 'r_g', 'r_m', 'r_m', 'r_t', 'd_i']
+quantity_list = ['d', 'r_t', 's', 'l', 't', 'r_g', 'r_m', 'd_i']
 
+prefix = 'hadoop jar '
+prefix += '/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-mr1.jar '
+prefix += '-D mapred.reduce.tasks=1 -file mapreducer.py'
 for quantity in quantity_list:
-    print 'hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-mr1.jar -D mapred.reduce.tasks=1 -file mapreducer.py -mapper "mapreducer.py mapper %s" -reducer "mapreducer.py reducer U" -input /GPSDATA/part-00000 -output /GPS_MAPRED_Mar/POP/%s_raw' % (quantity, quantity)
-    print 'hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-mr1.jar -D mapred.reduce.tasks=1 -file mapreducer.py -mapper "mapreducer.py mapper %s" -reducer "mapreducer.py reducer" -input /GPSDATA/part-00000 -output /GPS_MAPRED_Mar/POP/%s' % (quantity, quantity)
+    print prefix,
+    print '-mapper "mapreducer.py mapper %s" -reducer "mapreducer.py reducer U"' % quantity,
+    print '-input /GPS_CL_MAR2016/part-00000 -output /GPS_MAPRED_Mar/POP/%s_raw' % quantity
