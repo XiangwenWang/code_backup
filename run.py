9c8ea7c9c188985aaf1dcf3003693241c3dd9eb0
Xiangwen Wang <wxw@vt.edu>
2016-04-20 16:45:56 -0400

    Old versions

--- run.py
+++
@@ -1,9 +0,0 @@
-quantity_list = ['d', 'r_t', 's', 'l', 't', 'r_g', 'r_m', 'd_i']
-
-prefix = 'hadoop jar '
-prefix += '/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-mr1.jar '
-prefix += '-D mapred.reduce.tasks=1 -file mapreducer.py'
-for quantity in quantity_list:
-    print prefix,
-    print '-mapper "mapreducer.py mapper %s" -reducer "mapreducer.py reducer U"' % quantity,
-    print '-input /GPS_CL_MAR2016/part-00000 -output /GPS_MAPRED_Mar/POP/%s_raw' % quantity
